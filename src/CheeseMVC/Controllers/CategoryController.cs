18d9e68da5dec8f818b75692d6bc9d00bc787930
OJ SHin <tellmejin@gmail.com>
2017-12-17 12:17:20 -0600

    done sigle database

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+}
